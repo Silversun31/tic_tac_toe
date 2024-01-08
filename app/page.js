7a5bd2c493b5d31e7ac8cc4fdb1e1efb26e46ea5
silversun31 <silversunr@gmail.com>
2024-01-08 16:58:16 -0500

    Crear juego tic tac toe

--- app/page.js
+++ app/page.js
@@ -1,113 +1,212 @@
-import Image from 'next/image'
+'use client';
+import React, { useState, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
 
-export default function Home() {
+const rowStyle = {
+  display: 'flex'
+}
+
+const squareStyle = {
+  'width':'60px',
+  'height':'60px',
+  'backgroundColor': '#ddd',
+  'margin': '4px',
+  'display': 'flex',
+  'justifyContent': 'center',
+  'alignItems': 'center',
+  'fontSize': '20px',
+  'color': 'white'
+}
+
+const boardStyle = {
+  'backgroundColor': '#eee',
+  'width': '208px',
+  'alignItems': 'center',
+  'justifyContent': 'center',
+  'display': 'flex',
+  'flexDirection': 'column',
+  'border': '3px #eee solid'
+}
+
+const containerStyle = {
+  'display': 'flex',
+  'alignItems': 'center',
+  'flexDirection': 'column'
+}
+
+const instructionsStyle = {
+  'marginTop': '5px',
+  'marginBottom': '5px',
+  'fontWeight': 'bold',
+  'fontSize': '16px',
+}
+
+const buttonStyle = {
+  'marginTop': '15px',
+  'marginBottom': '16px',
+  'width': '80px',
+  'height': '40px',
+  'backgroundColor': '#8acaca',
+  'color': 'white',
+  'fontSize': '16px',
+}
+
+function Square({playingPlayer,setPlayingPlayer,newGame,setNewGame,plays,setPlays,row,col,anyWinner}) {
+  // Reset squares by calling useEffect on newGame
+  useEffect(()=>{
+    if (newGame){
+      setClear(true)
+      }
+    }, [newGame])
+  
+  // Tracking if square is clear or not
+  const [clear, setClear] = useState(true)
+
+  // Handle square click, change square text and set clear on false
+  const handleSquareClick = function (){
+    if (clear && !(anyWinner)){
+      let arra = plays
+      arra[row][col] = playingPlayer
+      setPlays(arra)
+      if (playingPlayer === 'X'){
+        setPlayingPlayer('O')
+      }
+      else if (playingPlayer === 'O'){
+        setPlayingPlayer('X')
+      }
+    setClear(false)
+    }
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div
+      className="square"
+      style={squareStyle}
+      onClick={handleSquareClick}>
+      {plays[row][col]}
+    </div>
+  );
+}
+
+function Board() {
+  // Tracking game plays for every player
+  const [plays, setPlays] = useState([['','',''],['','',''],['','','']])
+
+  //Tracking winner
+  const [winner, setWinner] = useState(null)
+
+  // Defining function for checking winner
+  function checkWinner(){
+    if(plays[0][0] === 'X' && plays[0][1] === 'X' && plays [0][2] === 'X'){
+      return 'X'
+    }
+    else if(plays[0][0] === 'O' && plays[0][1] === 'O' && plays [0][2] === 'O'){
+      return 'O'
+    }
+    else if(plays[1][0] === 'X' && plays[1][1] === 'X' && plays [1][2] === 'X'){
+      return 'X'
+    }
+    else if(plays[1][0] === 'O' && plays[1][1] === 'O' && plays [1][2] === 'O'){
+      return 'O'
+    }
+    else if(plays[2][0] === 'X' && plays[2][1] === 'X' && plays [2][2] === 'X'){
+      return 'X'
+    }
+    else if(plays[2][0] === 'O' && plays[2][1] === 'O' && plays [2][2] === 'O'){
+      return 'O'
+    }
+    else if(plays[0][0] === 'X' && plays[1][0] === 'X' && plays [2][0] === 'X'){
+      return 'X'
+    }
+    else if(plays[0][0] === 'O' && plays[1][0] === 'O' && plays [2][0] === 'O'){
+      return 'O'
+    }
+    else if(plays[0][1] === 'X' && plays[1][1] === 'X' && plays [2][1] === 'X'){
+      return 'X'
+    }
+    else if(plays[0][1] === 'O' && plays[1][1] === 'O' && plays [2][1] === 'O'){
+      return 'O'
+    }
+    else if(plays[0][2] === 'X' && plays[1][2] === 'X' && plays [2][2] === 'X'){
+      return 'X'
+    }
+    else if(plays[0][2] === 'O' && plays[1][2] === 'O' && plays [2][2] === 'O'){
+      return 'O'
+    }
+    else if(plays[0][0] === 'X' && plays[1][1] === 'X' && plays [2][2] === 'X'){
+      return 'X'
+    }
+    else if(plays[0][0] === 'O' && plays[1][1] === 'O' && plays [2][2] === 'O'){
+      return 'O'
+    }
+    else if(plays[0][2] === 'X' && plays[1][1] === 'X' && plays [2][0] === 'X'){
+      return 'X'
+    }
+    else if(plays[0][2] === 'O' && plays[1][1] === 'O' && plays [2][0] === 'O'){
+      return 'O'
+    }
+    return null
+  }
+
+  // Tracking playing player
+  const [playingPlayer, setPlayingPlayer] = useState('X')
+
+  // Tracking if game is new or not
+  const [newGame, setNewGame] = useState(true)
+
+  //Tracking if is any winner
+  const [anyWinner, setAnyWinner] = useState(false)
+
+  // Reset game by calling useEffect on newGame
+  useEffect(()=>{
+    if (newGame){
+      setNewGame(false)
+      setPlayingPlayer('X')
+      setPlays([['','',''],['','',''],['','','']])
+      setWinner(null)
+      setAnyWinner(false)
+      }
+    }, [newGame])
+  
+  //Checking winner for every play
+  useEffect(()=>{
+    if (checkWinner()){
+      setAnyWinner(true)
+      setWinner(checkWinner())
+      }
+    }, [playingPlayer])
+  
+  return (
+    <div style={containerStyle} className="gameBoard">
+      <div id="statusArea" className="status" style={instructionsStyle}>Next player: <span>{playingPlayer}</span></div>
+      <div id="winnerArea" className="winner" style={instructionsStyle}>Winner: <span>{winner?winner:'None'}</span></div>
+      <button style={buttonStyle} onClick={()=>{setNewGame(true)}}>Reset</button>
+      <div style={boardStyle}>
+        <div className="board-row" style={rowStyle}>
+          <Square playingPlayer={playingPlayer} setPlayingPlayer={setPlayingPlayer} newGame={newGame} setNewGame={setNewGame} plays={plays} setPlays={setPlays} anyWinner={anyWinner} row={0} col={0}/>
+          <Square playingPlayer={playingPlayer} setPlayingPlayer={setPlayingPlayer} newGame={newGame} setNewGame={setNewGame} plays={plays} setPlays={setPlays} anyWinner={anyWinner} row={0} col={1}/>
+          <Square playingPlayer={playingPlayer} setPlayingPlayer={setPlayingPlayer} newGame={newGame} setNewGame={setNewGame} plays={plays} setPlays={setPlays} anyWinner={anyWinner} row={0} col={2}/>
+        </div>
+        <div className="board-row" style={rowStyle}>
+          <Square playingPlayer={playingPlayer} setPlayingPlayer={setPlayingPlayer} newGame={newGame} setNewGame={setNewGame} plays={plays} setPlays={setPlays} anyWinner={anyWinner} row={1} col={0}/>
+          <Square playingPlayer={playingPlayer} setPlayingPlayer={setPlayingPlayer} newGame={newGame} setNewGame={setNewGame} plays={plays} setPlays={setPlays} anyWinner={anyWinner} row={1} col={1}/>
+          <Square playingPlayer={playingPlayer} setPlayingPlayer={setPlayingPlayer} newGame={newGame} setNewGame={setNewGame} plays={plays} setPlays={setPlays} anyWinner={anyWinner} row={1} col={2}/>
+        </div>
+        <div className="board-row" style={rowStyle}>
+          <Square playingPlayer={playingPlayer} setPlayingPlayer={setPlayingPlayer} newGame={newGame} setNewGame={setNewGame} plays={plays} setPlays={setPlays} anyWinner={anyWinner} row={2} col={0}/>
+          <Square playingPlayer={playingPlayer} setPlayingPlayer={setPlayingPlayer} newGame={newGame} setNewGame={setNewGame} plays={plays} setPlays={setPlays} anyWinner={anyWinner} row={2} col={1}/>
+          <Square playingPlayer={playingPlayer} setPlayingPlayer={setPlayingPlayer} newGame={newGame} setNewGame={setNewGame} plays={plays} setPlays={setPlays} anyWinner={anyWinner} row={2} col={2}/>
         </div>
       </div>
+    </div>
+  );
+}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
